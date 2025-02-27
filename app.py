82fd59441e66508d1c3360f29b0382f8b08719ca
katungyebrendan <145586135+katungyebrendan@users.noreply.github.com>
2025-02-27 15:23:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 
 # FastAPI prediction endpoint URL
-FASTAPI_PREDICT_URL = "http://127.0.0.1:8000/predict"
+FASTAPI_PREDICT_URL = "https://eastcoast-fastapi.onrender.com/predict"
 
 st.title("East Coast Fever Detection")
 st.write("Upload your cow health data or enter features manually to get prediction results.")
