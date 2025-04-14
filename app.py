214f378f7aa4fc6011e6736691fddd0bbf5fec9a
katungyebrendan <145586135+katungyebrendan@users.noreply.github.com>
2025-04-14 19:35:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 
 # FastAPI prediction endpoint URL
-FASTAPI_PREDICT_URL = "https://eastcoast-93xm.onrender.com/predict"
+FASTAPI_PREDICT_URL = "https://tiknet-b2da88db-b95a-449b-9752.ahumain.cranecloud.io"
 
 st.title("East Coast Fever Detection")
 st.write("Upload your cow health data or enter features manually to get prediction results.")
